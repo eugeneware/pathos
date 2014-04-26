93947e9e5759b2f4584c086b459593a6346f6f67
Eugene Ware <eugene@noblesamurai.com>
2014-04-26 16:42:41 +1000

    set/visit/rename/rewrite

--- test/index.js
+++ test/index.js
@@ -141,6 +141,264 @@ describe('pathos', function() {
 
     var result = pathos.build(paths);
     expect(result).to.eql(o);
+  it('should be able to set an object field with a path and value',
+    function(done) {
+      var o = {
+        name: 'Eugene',
+        number: 42,
+        tags: ['tag1', 'tag2', 'tag3'],
+        cars: [
+          {
+            make: 'Toyota',
+            model: 'Camry'
+          },
+          {
+            make: 'Toyota',
+            model: 'Corolla'
+          }
+        ]
+      };
+
+      pathos.set(o, [ 'cars', '1', 'make' ], 'Toyoda');
+      var expected1 = {
+        name: 'Eugene',
+        number: 42,
+        tags: ['tag1', 'tag2', 'tag3'],
+        cars: [
+          {
+            make: 'Toyota',
+            model: 'Camry'
+          },
+          {
+            make: 'Toyoda',
+            model: 'Corolla'
+          }
+        ]
+      };
+      expect(o).to.eql(expected1);
+
+      pathos.set(o, [ 'cars', '1', 'make' ], { old_name: 'Toyota', new_name: 'Toyota' });
+      var expected2 = {
+        name: 'Eugene',
+        number: 42,
+        tags: ['tag1', 'tag2', 'tag3'],
+        cars: [
+          {
+            make: 'Toyota',
+            model: 'Camry'
+          },
+          {
+            make: { old_name: 'Toyota', new_name: 'Toyota' },
+            model: 'Corolla'
+          }
+        ]
+      };
+      expect(o).to.eql(expected2);
+
+      done();
+    });
+
+  it('should handle setting of new properties', function(done) {
+    var o = {
+      nothing: 'here'
+    }
+    pathos.set(o, [ 'cars', '1', 'make' ], 'Toyoda');
+    var expected = {
+      nothing: 'here',
+      cars: {
+        '1': {
+          make: 'Toyoda'
+        }
+      }
+    };
+    expect(o).to.eql(expected);
+    done();
+  });
+
+  it('should be able to visit every key-value pair', function(done) {
+    var o = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyota',
+          model: 'Camry'
+        },
+        {
+          make: 'Toyota',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    var paths = [];
+    function visit(key, value) {
+      paths.push({ key: key, value: value });
+    }
+
+    pathos.visit(o, visit);
+    expect(paths).to.eql(
+      [ { key: [ 'name' ], value: 'Eugene' },
+        { key: [ 'number' ], value: 42 },
+        { key: [ 'tags', '0' ], value: 'tag1' },
+        { key: [ 'tags', '1' ], value: 'tag2' },
+        { key: [ 'tags', '2' ], value: 'tag3' },
+        { key: [ 'cars', '0', 'make' ], value: 'Toyota' },
+        { key: [ 'cars', '0', 'model' ], value: 'Camry' },
+        { key: [ 'cars', '1', 'make' ], value: 'Toyota' },
+        { key: [ 'cars', '1', 'model' ], value: 'Corolla' } ]);
+    done();
+  });
+
+  it('should be able to rename a field', function(done) {
+    var o = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyota',
+          model: 'Camry'
+        },
+        {
+          make: 'Toyota',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    function rename(key, value) {
+      if (key[key.length - 1] === 'make') {
+        key[key.length - 1] = 'manufacturer';
+        return {
+          key: key,
+          value: value
+        };
+      } else {
+        return true;
+      }
+    }
+
+    pathos.rewrite(o, rename);
+    var expected = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          manufacturer: 'Toyota',
+          model: 'Camry'
+        },
+        {
+          manufacturer: 'Toyota',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    expect(o).to.eql(expected);
+
+    done();
+  });
+
+  it('should be able to change a fields\'s value', function(done) {
+    var o = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyota',
+          model: 'Camry'
+        },
+        {
+          make: 'Toyota',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    function change(key, value) {
+      if (key[key.length - 1] === 'make') {
+        return {
+          key: key,
+          value: 'Toyoda'
+        };
+      } else {
+        return true;
+      }
+    }
+
+    pathos.rewrite(o, change);
+    var expected = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyoda',
+          model: 'Camry'
+        },
+        {
+          make: 'Toyoda',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    expect(o).to.eql(expected);
+
+    done();
+  });
+
+  it('should be able to remove a field', function(done) {
+    var o = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyota',
+          model: 'Camry'
+        },
+        {
+          make: 'Toyota',
+          model: 'Corolla'
+        }
+      ]
+    };
+
+    var expected = {
+      name: 'Eugene',
+      number: 42,
+      tags: ['tag1', 'tag2', 'tag3'],
+      cars: [
+        {
+          make: 'Toyota'
+        },
+        {
+          make: 'Toyota'
+        }
+      ]
+    };
+
+    function remove1(key, value) {
+      return key[key.length - 1] !== 'model';
+    }
+    pathos.rewrite(o, remove1);
+    expect(o).to.eql(expected);
+
+    function remove2(key, value) {
+      if (key[key.length - 1] === 'model') {
+        return {
+          key: key
+        };
+      }
+      return true;
+    }
+    pathos.rewrite(o, remove2);
+    expect(o).to.eql(expected);
 
     done();
   });
